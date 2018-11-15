4e7520bc345bcba2fed334c75401bd9257150ed9
Black Dots <igolovac629@gmail.com>
2018-11-15 18:32:55 +0200

    fixed style

--- my-app/src/components/Square.js
+++ my-app/src/components/Square.js
@@ -1,10 +1,9 @@
-import React from 'react';
-
 function Square(props) {
-    return (
-      <button className="square" onClick={props.onClick}>
-        {props.value}
-      </button>
-    );
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
+
 export default Square;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,5 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Game from './components/Game';
   
 ReactDOM.render(
   <Game />,
